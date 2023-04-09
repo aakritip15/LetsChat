9840bceecf963fb12cb3af6f2e06cd3b33f3b9ee
Aakriti Poudel <aakriti.p.03@gmail.com>
2023-04-09 22:33:12 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:lets_chat/screens/welcome_screen.dart';
+import 'package:lets_chat/screens/login_screen.dart';
+import 'package:lets_chat/screens/registration_screen.dart';
+import 'package:lets_chat/screens/chat_screen.dart';
+
+void main() => runApp(Lets_Chat());
+
+class Lets_Chat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData.dark().copyWith(
+          textTheme: TextTheme(
+            bodyLarge: TextStyle(color: Colors.black54),
+          ),
+        ),
+        debugShowCheckedModeBanner: false,
+        //home: WelcomeScreen(),
+        initialRoute: ChatScreen.id,
+        routes: {
+          WelcomeScreen.id: (context) => WelcomeScreen(),
+          RegistrationScreen.id: (context) => RegistrationScreen(),
+          ChatScreen.id: (context) => ChatScreen(),
+          LoginScreen.id: (context) => LoginScreen(),
+        });
+  }
+}
